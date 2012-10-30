f978cd901baf32cb3fac265b2805d884c611d0d5
root <root@koprad.(none)>
2012-10-30 11:39:30 +0100

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysql::Application.config.secret_token = '30f2f64c4bdc0d8129c78371ff867166d8446c2121ab24e9e11aa444c8f4032fc3bca892b27328a11ab70eabc749ce7a8ac1eb71470efbe078ff42d669a62406'
